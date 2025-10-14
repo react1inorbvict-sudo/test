16fc819547ef63d2e4c7e70ec74b8671062d4eca
react1inorbvict-sudo <react1.inorbvict@gmail.com>
2025-10-14 11:26:24 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>ℹ️ About Page</h1>
+      <p>This app demonstrates basic React routing and components.</p>
+    </div>
+  );
+}
+
+export default About;
